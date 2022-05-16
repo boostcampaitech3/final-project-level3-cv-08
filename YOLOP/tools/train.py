e536c1bd480266f20b900017639d7efe7a3328e5
j8n17 <j3837301@gmail.com>
2022-05-16 17:17:06 +0000

    don't work logger.info

--- YOLOP/tools/train.py
+++ YOLOP/tools/train.py
@@ -417,7 +417,7 @@ def main():
                         output_dir=os.path.join(cfg.LOG_DIR, cfg.DATASET.DATASET),
                         filename='model_best_train_all.pth'
                     )
-                    logger.info(f"=> saving best model_epoch{epoch}...")
+                    print(f"=> saving best model_epoch{epoch}...")
             # if you train without seg, save checkpoint when det_mAP is best
             train_without_seg = cfg.TRAIN.ENC_DET_ONLY == True and cfg.TRAIN.SEG_ONLY == False and cfg.TRAIN.DET_ONLY == False and cfg.TRAIN.ENC_SEG_ONLY == False and cfg.TRAIN.DRIVABLE_ONLY == False and cfg.TRAIN.LANE_ONLY == False
 
@@ -435,7 +435,7 @@ def main():
                         output_dir=os.path.join(cfg.LOG_DIR, cfg.DATASET.DATASET),
                         filename='model_best_train_enc_det_only.pth'
                     )
-                    logger.info(f"=> saving best model_epoch{epoch}...")
+                    print(f"=> saving best model_epoch{epoch}...")
             # if you train only seg branches, save checkpoint when seg_mIoU are best
             train_only_seg = cfg.TRAIN.SEG_ONLY == True and cfg.TRAIN.ENC_DET_ONLY == False and cfg.TRAIN.DET_ONLY == False and cfg.TRAIN.ENC_SEG_ONLY == False and cfg.TRAIN.DRIVABLE_ONLY == False and cfg.TRAIN.LANE_ONLY == False
 
@@ -453,7 +453,7 @@ def main():
                         output_dir=os.path.join(cfg.LOG_DIR, cfg.DATASET.DATASET),
                         filename='model_best_train_seg_only.pth'
                     )
-                    logger.info(f"=> saving best model_epoch{epoch}...")
+                    print(f"=> saving best model_epoch{epoch}...")
         sec = time.time() - start
         result = datetime.timedelta(seconds=sec)
         print(f'------- {result} -------')
