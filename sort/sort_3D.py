rotation issue fixed perfectly

sort/sort_3D.py
@@ -32,7 +32,101 @@ import argparse
 import cv2
 from filterpy.kalman import KalmanFilter
 
+######################################kalman filter를 위한 import
+from copy import deepcopy
+from math import log, exp, sqrt
+import sys
+import numpy as np
+from numpy import dot, zeros, eye, isscalar, shape
+import numpy.linalg as linalg
+from filterpy.stats import logpdf
+from filterpy.common import pretty_str, reshape_z
+######################################
+
 np.random.seed(0)
+
+class KalmanFilterCustom(KalmanFilter):
+    def __init__(self, dim_x, dim_z, dim_u=0):
+        super().__init__(dim_x, dim_z)
+
+    def update(self, z, R=None, H=None):
+        """
+        Add a new measurement (z) to the Kalman filter.
+        If z is None, nothing is computed. However, x_post and P_post are
+        updated with the prior (x_prior, P_prior), and self.z is set to None.
+        Parameters
+        ----------
+        z : (dim_z, 1): array_like
+            measurement for this update. z can be a scalar if dim_z is 1,
+            otherwise it must be convertible to a column vector.
+            If you pass in a value of H, z must be a column vector the
+            of the correct size.
+        R : np.array, scalar, or None
+            Optionally provide R to override the measurement noise for this
+            one call, otherwise  self.R will be used.
+        H : np.array, or None
+            Optionally provide H to override the measurement function for this
+            one call, otherwise self.H will be used.
+        """
+
+        # set to None to force recompute
+        self._log_likelihood = None
+        self._likelihood = None
+        self._mahalanobis = None
+
+        if z is None:
+            self.z = np.array([[None]*self.dim_z]).T
+            self.x_post = self.x.copy()
+            self.P_post = self.P.copy()
+            self.y = zeros((self.dim_z, 1))
+            return
+
+        if R is None:
+            R = self.R
+        elif isscalar(R):
+            R = eye(self.dim_z) * R
+
+        if H is None:
+            z = reshape_z(z, self.dim_z, self.x.ndim)
+            H = self.H
+
+        # y = z - Hx
+        # error (residual) between measurement and prediction
+        self.y = z - dot(H, self.x)
+        if abs(self.y[2][0]) > np.pi:
+            if self.y[2][0] > 0 :
+                self.y[2][0] = abs(self.y[2][0]) - 2*np.pi
+            else:
+                self.y[2][0] = 2*np.pi - abs(self.y[2][0])
+
+        # common subexpression for speed
+        PHT = dot(self.P, H.T)
+
+        # S = HPH' + R
+        # project system uncertainty into measurement space
+        self.S = dot(H, PHT) + R
+        self.SI = self.inv(self.S)
+        # K = PH'inv(S)
+        # map system uncertainty into kalman gain
+        self.K = dot(PHT, self.SI)
+
+        # x = x + Ky
+        # predict new x with residual scaled by the kalman gain
+        self.x = self.x + dot(self.K, self.y)
+
+        # P = (I-KH)P(I-KH)' + KRK'
+        # This is more numerically stable
+        # and works for non-optimal K vs the equation
+        # P = (I-KH)P usually seen in the literature.
+
+        I_KH = self._I - dot(self.K, H)
+        self.P = dot(dot(I_KH, self.P), I_KH.T) + dot(dot(self.K, R), self.K.T)
+
+        # save measurement and posterior state
+        self.z = deepcopy(z)
+        self.x_post = self.x.copy()
+        self.P_post = self.P.copy()
+    
 
 def linear_assignment(cost_matrix):
     try:
@@ -102,7 +196,7 @@ class KalmanBoxTracker(object):
         """
         #define constant velocity model
         self.cls_id = bbox[6]
-        self.kf = KalmanFilter(dim_x=8, dim_z=5)
+        self.kf = KalmanFilterCustom(dim_x=8, dim_z=5)
         self.kf.F = np.array([[1,0,0,0,0,1,0,0],
                             [0,1,0,0,0,0,1,0],
                             [0,0,1,0,0,0,0,1],