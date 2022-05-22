bac89714561025e20fb279ea01787fee85731e96
haymrpig <nevermail_@naver.com>
2022-05-22 04:17:58 +0000

    fps increased code

--- mmdetection3d/mmdet3d/apis/test.py
+++ mmdetection3d/mmdet3d/apis/test.py
@@ -1,13 +1,14 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from os import path as osp
 
-
+import time
 from PIL import Image
 import mmcv
 import torch
 import numpy as np
 from mmcv.image import tensor2imgs
 import cv2
+import os
 
 from mmdet3d.models import (Base3DDetector, Base3DSegmentor,
                             SingleStageMono3DDetector)
@@ -15,7 +16,7 @@ from mmdet3d.models import (Base3DDetector, Base3DSegmentor,
 from utils.utils import lidar2Bev, Sort, detection2Bev, bevPoints, drawBbox, transformImg
 from lib.core.general import non_max_suppression, scale_coords
 from lib.utils import plot_one_box,show_seg_result
-
+from ..core.evaluation.kitti_utils.rotate_iou import rotate_iou_gpu_eval
 
 def single_gpu_test(model,
                     model_2d,
@@ -49,74 +50,86 @@ def single_gpu_test(model,
     colors = [[np.random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
 
     results = []
+    bev_results = []
     dataset = data_loader.dataset
     prog_bar = mmcv.ProgressBar(len(dataset))
 
     mot_tracker = Sort(max_age=args.max_age, 
                        min_hits=args.min_hits,
                        centerpoint_threshold=args.centerpoint_threshold) #create instance of the SORT tracker
-    for i, data in enumerate(data_loader):
-        with torch.no_grad():
-            result = model(return_loss=False, rescale=True, **data)
-        
-        # 모든 lidar BEV view
-        velodyne_path = dataset[i]['img_metas'][0].data['pts_filename']
-        top, density_image = lidar2Bev(velodyne_path)
+    
+    if not os.path.exists('/opt/ml/tracking'):
+        os.makedirs('/opt/ml/tracking')
+    with open('/opt/ml/tracking/tracking.txt', 'w') as tracking_file:
+        print(f'if using cv2 to draw points -> x,y order : cv2.line(..., pt1=(x,y), pt2=(x,y), ...)', file=tracking_file)
+        print(f'frame, tracking_id, x(garo), y(sero), class_id', file=tracking_file)
+        for i, data in enumerate(data_loader):
+            with torch.no_grad():
+                result = model(return_loss=False, rescale=True, **data)
 
-        # 모든 detection BEV view
-        # total_det는 n x 7 numpy array -> x, y, rot, h, w, score, cls_id
-        total_det = detection2Bev(result[0], i)
+            # 모든 lidar BEV view
+            velodyne_path = dataset[i]['img_metas'][0].data['pts_filename']
+            top, density_image = lidar2Bev(velodyne_path)
+            
+            # 모든 detection BEV view
+            # total_det는 n x 7 numpy array -> x, y, rot, h, w, score, cls_id
+            total_det = detection2Bev(result[0], i)
 
-        # tracking
-        trackers = mot_tracker.update(total_det)
-        # x,y,rot,h,w -> x1,y1,x2,y2,x3,y3,x4,y4
-        rotated_points = bevPoints(trackers)
+            # tracking
+            trackers = mot_tracker.update(total_det)
+            # x,y,rot,h,w -> x1,y1,x2,y2,x3,y3,x4,y4
 
-        # draw bbox on bev lidar points
-        density_image = drawBbox(rotated_points, trackers, density_image, i)
+            rotated_points= bevPoints(trackers)
 
-        cv2.imwrite(f'/opt/ml/images/3D/image{i:06d}.png', density_image)
-        
+            # draw bbox on bev lidar points
+            density_image = drawBbox(rotated_points, trackers, density_image, i, tracking_file)
 
-        """
-        YOLOP start
-        """
-        img_path = velodyne_path.replace('velodyne', 'image_2').replace('bin', 'png')
-        img = Image.open(img_path).resize((640, 384))
-        img_det = np.array(img)
+            cv2.imwrite(f'/opt/ml/images/3D/image{i:06d}.png', density_image)
 
-        if torch.cuda.is_available():
-            img = transformImg(img).unsqueeze(0).cuda()
-        else:
-            img = transformImg(img).unsqueeze(0).cpu()
+            """
+            YOLOP start
+            """
+            img_path = velodyne_path.replace('velodyne', 'image_2').replace('bin', 'png')
+            img = Image.open(img_path).resize((640, 384))
+            img_det = np.array(img)
+            
 
-        det_out, da_seg_out, ll_seg_out = model_2d(img)
-        inf_out, _ = det_out
-        det_pred = non_max_suppression(inf_out, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False)
-        det=det_pred[0]
+            if torch.cuda.is_available():
+                img = transformImg(img).unsqueeze(0).cuda()
+            else:
+                img = transformImg(img).unsqueeze(0).cpu()
 
-        _, _, height, width = img.shape
-        h,w,_=img_det.shape
 
-        _, da_seg_out = torch.max(da_seg_out, 1)
-        da_seg_out = da_seg_out.int().squeeze().cpu().numpy()
+            with torch.no_grad():
+                det_out, da_seg_out, ll_seg_out = model_2d(img)
 
-        _, ll_seg_out = torch.max(ll_seg_out, 1)
-        ll_seg_out = ll_seg_out.int().squeeze().cpu().numpy()
-        
-        img_det = show_seg_result(img_det, (da_seg_out, ll_seg_out), _, _, is_demo=True)
+            inf_out, _ = det_out
+            det_pred = non_max_suppression(inf_out, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False)
+            det=det_pred[0]
 
-        if len(det):
-            det[:,:4] = scale_coords(img.shape[2:],det[:,:4],img_det.shape).round()
-            for *xyxy,conf,cls in reversed(det):
-                label_det_pred = f'{names[int(cls)]} {conf:.2f}'
-                plot_one_box(xyxy, img_det , label=label_det_pred, color=colors[int(cls)], line_thickness=2)
-        
-        cv2.imwrite(f'/opt/ml/images/2D/image{i:06d}.png', img_det)
+            _, _, height, width = img.shape
+            h,w,_=img_det.shape
 
-        results.extend(result)
+            _, da_seg_out = torch.max(da_seg_out, 1)
+            da_seg_out = da_seg_out.int().squeeze().cpu().numpy()
 
-        batch_size = len(result)
-        for _ in range(batch_size):
-            prog_bar.update()
-    return results
+            _, ll_seg_out = torch.max(ll_seg_out, 1)
+            ll_seg_out = ll_seg_out.int().squeeze().cpu().numpy()
+            
+
+            img_det = show_seg_result(img_det, (da_seg_out, ll_seg_out), _, _, is_demo=True)
+            if len(det):
+                det[:,:4] = scale_coords(img.shape[2:],det[:,:4],img_det.shape).round()
+                for *xyxy,conf,cls in reversed(det):
+                    label_det_pred = f'{names[int(cls)]} {conf:.2f}'
+                    plot_one_box(xyxy, img_det , label=label_det_pred, color=colors[int(cls)], line_thickness=2)
+            
+            
+            cv2.imwrite(f'/opt/ml/images/2D/image{i:06d}.png', img_det)
+
+            results.extend(result)
+            bev_results.extend(total_det)
+            batch_size = len(result)
+            for _ in range(batch_size):
+                prog_bar.update()
+    return results, bev_results
