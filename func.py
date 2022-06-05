e40c8afb8388269391ff168f2bd9f9b2170c5639
zzundi <dlwnsgur0803@naver.com>
2022-06-05 19:00:55 +0000

    lane projection

---
+++ func.py
@@ -0,0 +1,365 @@
+"""
+ @leofansq
+
+ Basic function:
+    show_img(name, img): Show the image
+    find_files(directory, pattern): Method to find target files in one directory, including subdirectory
+ Load function:
+    load_calib_cam2cam(filename, debug=False): Only load R_rect & P_rect for need
+    load_calib_lidar2cam(filename, debug=False): Load calib parameters for LiDAR2Cam
+    load_calib(filename, debug=False): Load the calib parameters which has R_rect & P_rect & Tr in the same file
+    load_img(filename, debug=False): Load the image
+    load_lidar(filename, debug=False): Load the PointCloud
+ Process function:
+    cal_proj_matrix_raw(filename_c2c, filename_l2c, camera_id, debug=False): Compute the projection matrix from LiDAR to Img
+    cal_proj_matrix(filename, camera_id, debug=False): Compute the projection matrix from LiDAR to Image
+    project_lidar2img(img, pc, p_matrix, debug=False): Project the LiDAR PointCloud to Image
+    generate_colorpc(img, pc, pcimg, debug=False): Generate the PointCloud with color
+    save_pcd(filename, pc_color): Save the PointCloud with color in the term of .pcd
+"""
+import cv2
+import numpy as np
+from pyntcloud import PyntCloud
+
+import os
+import fnmatch
+from tqdm import tqdm
+from pprint import pprint
+
+#**********************************************************#
+#                    Basic Function                        #
+#**********************************************************#
+
+def show_img(name, img):
+    """
+    Show the image
+
+    Parameters:    
+        name: name of window    
+        img: image
+    """
+    cv2.namedWindow(name, 0)
+    cv2.imshow(name, img)
+
+def find_files(directory, pattern):
+    """
+    Method to find target files in one directory, including subdirectory
+    :param directory: path
+    :param pattern: filter pattern
+    :return: target file path list
+    """
+    file_list = []
+    for root, _, files in os.walk(directory):
+        for basename in files:
+            if fnmatch.fnmatch(basename, pattern):
+                filename = os.path.join(root, basename)
+                file_list.append(filename)
+    
+    return file_list
+
+#**********************************************************#
+#                     Load Function                        #
+#**********************************************************#
+
+def load_calib_cam2cam(filename, debug=False):
+    """
+    Only load R_rect & P_rect for neeed
+    Parameters: filename of the calib file
+    Return: 
+        R_rect: a list of r_rect(shape:3*3)
+        P_rect: a list of p_rect(shape:3*4)
+    """
+    with open(filename) as f_calib:
+        lines = f_calib.readlines()
+    
+    R_rect = []
+    P_rect = []
+
+    for line in lines:
+        title = line.strip().split(' ')[0]
+        if title[:-4] == "R_rect":
+            r_r = np.array(line.strip().split(' ')[1:], dtype=np.float32)
+            r_r = np.reshape(r_r, (3,3))
+            R_rect.append(r_r)
+        elif title[:-4] == "P_rect":
+            p_r = np.array(line.strip().split(' ')[1:], dtype=np.float32)
+            p_r = np.reshape(p_r, (3,4))
+            P_rect.append(p_r)
+    
+    if debug:
+        print ("R_rect:")
+        pprint (R_rect)
+
+        print ()
+        print ("P_rect:")
+        pprint (P_rect)
+    
+    return R_rect, P_rect
+
+def load_calib_lidar2cam(filename, debug=False):
+    """
+    Load calib
+    Parameters: filename of the calib file
+    Return:
+        tr: shape(4*4)
+            [  r   t
+             0 0 0 1]
+    """
+    with open(filename) as f_calib:
+        lines = f_calib.readlines()
+    
+    for line in lines:
+        title = line.strip().split(' ')[0]
+        if title[:-1] == "R":
+            r = np.array(line.strip().split(' ')[1:], dtype=np.float32)
+            r = np.reshape(r, (3,3))
+        if title[:-1] == "T":
+            t = np.array(line.strip().split(' ')[1:], dtype=np.float32)
+            t = np.reshape(t, (3,1))
+    
+    tr = np.hstack([r,t])
+    tr = np.vstack([tr,np.array([0,0,0,1])])
+
+    if debug:
+        print ()
+        print ("Tr:")
+        print (tr)
+
+    return tr
+
+def load_calib(filename, debug=False):
+    """
+    Load the calib parameters which has R_rect & P_rect & Tr in the same file
+    Parameters:
+        filename: the filename of the calib file
+    Return:
+        R_rect, P_rect, Tr
+    """
+    with open(filename) as f_calib:
+        lines = f_calib.readlines()
+    
+        P_rect = []    
+    for line in lines:
+        title = line.strip().split(' ')[0]
+        if len(title):
+            if title[0] == "R":
+                R_rect = np.array(line.strip().split(' ')[1:], dtype=np.float32)
+                R_rect = np.reshape(R_rect, (3,3))
+            elif title[0] == "P":
+                p_r = np.array(line.strip().split(' ')[1:], dtype=np.float32)
+                p_r = np.reshape(p_r, (3,4))
+                P_rect.append(p_r)
+            elif title[:-1] == "Tr_velo_to_cam":
+                Tr = np.array(line.strip().split(' ')[1:], dtype=np.float32)
+                Tr = np.reshape(Tr, (3,4))
+                Tr = np.vstack([Tr,np.array([0,0,0,1])])
+    
+    return R_rect, P_rect, Tr
+
+
+def load_img(filename, debug=False):
+    """
+    Load the image
+    Parameter:
+        filename: the filename of the image
+    Return:
+        img: image
+    """
+    img = cv2.imread(filename)
+    
+    if debug: show_img("Image", img)
+
+    return img
+
+def load_lidar(filename, debug=False):
+    """
+    Load the PointCloud
+    Parameter:
+        filename: the filename of the PointCloud
+    Return:
+        points: PointCloud associated with the image
+    """
+    # N*4 -> N*3
+    points = np.fromfile(filename, dtype=np.float32)
+    points = np.reshape(points, (-1,4))
+    points = points[:, :3]
+
+    points.tofile("./temp_pc.bin")
+
+    # Remove all points behind image plane (approximation)
+    cloud = PyntCloud.from_file("./temp_pc.bin")
+    cloud.points = cloud.points[cloud.points["x"]>=0]
+    points = np.array(cloud.points)
+    if debug:
+        print (points.shape)
+
+    return points
+
+#**********************************************************#
+#                   Process Function                       #
+#**********************************************************#
+
+def cal_proj_matrix_raw(filename_c2c, filename_l2c, camera_id, debug=False):
+    """
+    Compute the projection matrix from LiDAR to Img
+    Parameters:
+        filename_c2c: filename of the calib file for cam2cam
+        filename_l2c: filename of the calib file for lidar2cam
+        camera_id: the NO. of camera
+    Return:
+        P_lidar2img: the projection matrix from LiDAR to Img
+    """
+    # Load Calib Parameters
+    R_rect, P_rect = load_calib_cam2cam(filename_c2c, debug)
+    tr = load_calib_lidar2cam(filename_l2c, debug)
+
+    # Calculation
+    R_cam2rect = np.hstack([R_rect[0], np.array([[0],[0],[0]])])
+    R_cam2rect = np.vstack([R_cam2rect, np.array([0,0,0,1])])
+    
+    P_lidar2img = np.matmul(P_rect[camera_id], R_cam2rect)
+    P_lidar2img = np.matmul(P_lidar2img, tr)
+
+    if debug:
+        print ()
+        print ("P_lidar2img:")
+        print (P_lidar2img)
+
+    return P_lidar2img
+
+def cal_proj_matrix(filename, camera_id, debug=False):
+    """
+    Compute the projection matrix from LiDAR to Img
+    Parameters:
+        filename: filename of the calib file
+        camera_id: the NO. of camera
+    Return:
+        P_lidar2img: the projection matrix from LiDAR to Img
+    """
+    # Load Calib Parameters
+    R_rect, P_rect, tr = load_calib(filename, debug)
+
+    # Calculation
+    R_cam2rect = np.hstack([R_rect, np.array([[0],[0],[0]])])
+    R_cam2rect = np.vstack([R_cam2rect, np.array([0,0,0,1])])
+    
+    P_lidar2img = np.matmul(P_rect[camera_id], R_cam2rect)
+    P_lidar2img = np.matmul(P_lidar2img, tr)
+
+    if debug:
+        print ()
+        print ("P_lidar2img:")
+        print (P_lidar2img)
+
+    return P_lidar2img
+
+def project_lidar2img(img, pc, p_matrix, debug=False):
+    """
+    Project the LiDAR PointCloud to Image
+    Parameters:
+        img: Image
+        pc: PointCloud
+        p_matrix: projection matrix
+    """
+    # Dimension of data & projection matrix
+    dim_norm = p_matrix.shape[0]
+    dim_proj = p_matrix.shape[1]
+
+    # Do transformation in homogenuous coordinates
+    pc_temp = pc.copy()
+    if pc_temp.shape[1]<dim_proj:
+        pc_temp = np.hstack([pc_temp, np.ones((pc_temp.shape[0],1))])
+    points = np.matmul(p_matrix, pc_temp.T)
+    points = points.T
+
+    temp = np.reshape(points[:,dim_norm-1], (-1,1))
+    points = points[:,:dim_norm]/(np.matmul(temp, np.ones([1,dim_norm])))
+    
+    # Plot
+    if debug:
+        depth_max = np.max(pc[:,0])
+        for idx,i in enumerate(points):
+            color = int((pc[idx,0]/depth_max)*255)
+            cv2.rectangle(img, (int(i[0]-1),int(i[1]-1)), (int(i[0]+1),int(i[1]+1)), (0, 0, color), -1)
+        show_img("Test", img)
+    points = points[:, ]
+    return points
+
+def generate_colorpc(img, pc, pcimg, debug=False):
+    """
+    Generate the PointCloud with color
+    Parameters:
+        img: image
+        pc: PointCloud
+        pcimg: PointCloud project to image
+    Return:
+        pc_color: PointCloud with color e.g. X Y Z R G B
+    """
+    x = np.reshape(pcimg[:,0], (-1,1))
+    y = np.reshape(pcimg[:,1], (-1,1))
+    xy = np.hstack([x,y])
+
+    pc_color = []
+    for idx, i in enumerate(xy):
+        if (i[0]>1 and i[0]<img.shape[1]) and (i[1]>1 and i[1]<img.shape[0]): 
+            bgr = img[int(i[1]), int(i[0])]
+            p_color = [pc[idx][0], pc[idx][1], pc[idx][2], bgr[2], bgr[1], bgr[0]]
+            pc_color.append(p_color)
+    pc_color = np.array(pc_color)
+
+    return pc_color
+
+def save_pcd(filename, pc_color):
+    """
+    Save the PointCloud with color in the term of .pcd
+    Parameter:
+        filename: filename of the pcd file
+        pc_color: PointCloud with color
+    """
+    f = open(filename, "w")
+
+    f.write("# .PCD v0.7 - Point Cloud Data file format\n")
+    f.write("VERSION 0.7\n")
+    f.write("FIELDS x y z rgb\n")
+    f.write("SIZE 4 4 4 4\n")
+    f.write("TYPE F F F F\n")
+    f.write("COUNT 1 1 1 1\n")
+    f.write("WIDTH {}\n".format(pc_color.shape[0]))
+    f.write("HEIGHT 1\n")
+    f.write("POINTS {}\n".format(pc_color.shape[0]))
+    f.write("DATA ascii\n")
+
+    for i in pc_color:
+        # rgb = (int(i[3])<<16) | (int(i[4])<<8) | (int(i[5]) | 1<<24)
+        # f.write("{:.6f} {:.6f} {:.6f} {}\n".format(i[0],i[1],i[2],rgb))
+        f.write("{:.6f} {:.6f} {:.6f} {} {} {}\n".format(i[0],i[1],i[2],i[3],i[4],i[5]))
+    
+    f.close()
+
+
+if __name__ == '__main__':  
+    # Option
+    calib_cam2cam = "./calib/calib_cam_to_cam.txt"
+    calib_lidar2camera = "./calib/calib_velo_to_cam.txt"
+    camera_id = 1
+
+    filepath_img = "./img/000003.png"
+    # filepath_img = "./new.png"
+    filepath_lidar = "./lidar/000003.bin"
+    filename_save = "./test.pcd"
+
+    debug = True
+
+    # Process
+    p_matrix = cal_proj_matrix_raw(calib_cam2cam, calib_lidar2camera, camera_id, debug)
+    img = load_img(filepath_img, debug)
+    img = img[0:150,0:500]
+    pc = load_lidar(filepath_lidar, debug)
+    pcimg = project_lidar2img(img, pc, p_matrix, debug)
+    pc_color = generate_colorpc(img, pc, pcimg)
+    save_pcd(filename_save, pc_color)
+
+    if debug:
+        key = cv2.waitKey(0) & 0xFF
+        cv2.destroyAllWindows()
+
---
+++ lane_projection.py
@@ -0,0 +1,212 @@
+"""
+ @leofansq
+ Main function
+"""
+import cv2
+import numpy as np
+from PIL import Image, ImageDraw
+import os
+import time
+from tqdm import tqdm
+from sklearn import linear_model
+from func import find_files, cal_proj_matrix, cal_proj_matrix_raw, load_img, load_lidar, project_lidar2img
+
+#**********************************************************#
+#                         Option                           #
+#**********************************************************#
+################## FILE PATH ###################
+# Calib File
+CALIB_TYPE = 0      # 0:All parameters in one file. e.g. KITTI    1: Seperate into two files. e.g. KITTI raw
+# if CALIB_TYPE == 0
+CALIB = "./calib/000000.txt"
+# if CALIB_TYPE == 1    
+CAM2CAM = "./calib/calib_cam_to_cam.txt"
+LIDAR2CAM = "./calib/calib_velo_to_cam.txt"
+
+# Source File
+IMG_PATH = "/opt/ml/Tools_Merge_Image_PointCloud-1/lane_seg_image/test_image/"
+LIDAR_PATH = "/opt/ml/Tools_Merge_Image_PointCloud-1/lane_seg_image/test_velo/"
+
+# Save File
+#SIMG_PATH = "./result3/img/"
+SBEV_PATH = "./result3/bev/"
+
+################# PARAMETER ####################
+CAM_ID = 2
+
+# src = np.array([[0,0,1,0,0,1,0,0],[0,0,1,1,1,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0],[0,0,1,0,0,1,0,0]], dtype=np.float32)
+# print(cv2.resize(src, (1920, 1280)))
+# assert 1==0
+
+
+
+def add_square_feature(X):
+    X = np.concatenate([(X**2).reshape(-1,1), X], axis=1)
+    return X
+
+lane_t_num = 0
+#**********************************************************#
+#                     Main Function                        #
+#**********************************************************#
+def main():
+    global lane_xy
+    time_cost = []
+    
+    # Calculate P_matrix
+    if CALIB_TYPE:
+        p_matrix = cal_proj_matrix_raw(CAM2CAM, LIDAR2CAM, CAM_ID)
+    else:
+        p_matrix = cal_proj_matrix(CALIB, CAM_ID)
+
+    # Batch Process
+    for img_path in tqdm(find_files(IMG_PATH, '*.png')):
+        
+        _, img_name = os.path.split(img_path)
+        pc_path = LIDAR_PATH + img_name[:-4] + '.bin'
+        # print ("Working on", img_name[:-4])        
+        start_time = time.time()
+        # Load img & pc
+        img = load_img(img_path)
+        pc = load_lidar(pc_path)
+        
+        # Project & Generate Image & Save
+        points = project_lidar2img(img, pc, p_matrix)
+        
+        
+        
+        lane_coord = []
+        pc = pc[(points[:,0]<1242) & (1<points[:,0]) & (1<points[:,1]) & (points[:,1]<375)]
+        points = points[(points[:,0]<1242) & (1<points[:,0]) & (1<points[:,1]) & (points[:,1]<375)]
+
+        LANE_IMG = cv2.imread(IMG_PATH + img_name)
+        LANE_IMG = LANE_IMG[:,:,0]
+        LANE_IMG = np.array(LANE_IMG, dtype=np.float32)
+        LANE_IMG = cv2.resize(LANE_IMG, (1242, 375))
+
+        for idx,i in enumerate(points):
+            if LANE_IMG[int(i[1]),int(i[0])]:
+                lane_coord.append(idx)
+                
+        # Generate PC with Clor & Save
+        #pc_color = generate_colorpc(img, pc, points)
+        #save_pcd(SPC_PATH + img_name[:-4] + ".pcd", pc_color)
+        # BEV
+
+        img_bev = np.zeros((800, 700, 3))
+        # for idx,i in enumerate(pc):
+        #     # if idx in lane_coord:
+        #     #     img_bev[-int(i[0]*10)+799, int(-i[1]*10)+350] = [0, 0, 255]
+                
+            
+        #     if -int(i[0]*10)+799 < 800 and int(-i[1]*10)+350 <700:
+        #         img_bev[-int(i[0]*10)+799, int(-i[1]*10)+350] = [255, 255, 255]
+        lane_xy = []
+        for i in lane_coord:
+            lane_xy.append(list(pc[i,:2]))
+        lane_xy.sort()
+        lane_xy = np.array(lane_xy)
+        # for i in range(len(lane_xy)-1):
+        #     print(abs(lane_xy[i,1] - lane_xy[i+1,1]))
+        # assert 1==0
+        # print(lane_xy.shape)
+        
+        global lane_0
+        lane_0 = lane_xy[0:1]
+        def lane_classification(idx, lane_number, max_lane):
+            global lane_t_num
+            if abs(lane_xy[idx,1] - lane_xy[idx+1,1]) <= 1:
+                if len(lane_xy) > idx+2:
+                    globals()[f'lane_{lane_number}'] = np.vstack((globals()[f'lane_{lane_number}'], lane_xy[idx+1:idx+2]))
+                    lane_classification(idx+1, lane_number, max_lane)
+
+            elif len(lane_xy) > idx+2:
+                
+                s = 1
+                for i in range(max_lane+1):
+
+                    if abs(globals()[f'lane_{i}'][-1, 1] - lane_xy[idx+1, 1]) <= 1:
+
+                        globals()[f'lane_{i}'] = np.vstack((globals()[f'lane_{i}'], lane_xy[idx+1:idx+2]))
+                        s = 0
+                        lane_number = i
+                        break
+                if s:
+                    max_lane += 1
+                    globals()[f'lane_{max_lane}'] = lane_xy[idx+1:idx+2]
+                    lane_number = max_lane
+                    lane_t_num = max_lane + 1
+                lane_classification(idx+1, lane_number, max_lane)
+
+        
+        
+
+        lane_classification(0, 0, 0)
+        LANE = []
+        LANE_DICT = {}
+        
+        for i in range(lane_t_num):
+            if len(globals()[f'lane_{i}']) >= len(lane_xy)//20:
+                LANE.append(globals()[f'lane_{i}'])
+        for i in LANE:
+            LANE_DICT[abs(np.mean(i[:,1]))] = i
+        LANE_OURS = [LANE_DICT.pop(min(LANE_DICT)), LANE_DICT.pop(min(LANE_DICT))]
+        
+        # for lane_xy in LANE_OTHERS:
+        #     X = lane_xy[:,0]
+        #     y = lane_xy[:,1]
+        #     X = X.reshape(-1, 1)
+
+        #     # Fit line using all data
+        #     lr = linear_model.LinearRegression()
+        #     lr.fit(add_square_feature(X), y)
+
+        #     # Robustly fit linear model with RANSAC algorithm
+        #     ransac = linear_model.RANSACRegressor()
+        #     ransac.fit(add_square_feature(X), y)
+        #     inlier_mask = ransac.inlier_mask_
+        #     outlier_mask = np.logical_not(inlier_mask)
+
+        #     # Predict data of estimated models
+        #     line_X = np.arange(X.min(), X.max())[:, np.newaxis]
+        #     line_y_ransac = ransac.predict(add_square_feature(line_X))
+
+        #     for i in range(len(line_X)-1):
+        #         cv2.line(img_bev, (int(-line_y_ransac[i]*10)+350, -int(line_X[i]*10)+799), (int(-line_y_ransac[i+1]*10)+350, -int(line_X[i+1]*10)+799), (0,0,255), 3)
+       
+
+        
+       
+
+        for lane_xy in LANE_OURS:
+            X = lane_xy[:,0]
+            y = lane_xy[:,1]
+            X = X.reshape(-1, 1)
+
+            # Fit line using all data
+            lr = linear_model.LinearRegression()
+            lr.fit(add_square_feature(X), y)
+
+            # Robustly fit linear model with RANSAC algorithm
+            ransac = linear_model.RANSACRegressor()
+            ransac.fit(add_square_feature(X), y)
+            inlier_mask = ransac.inlier_mask_
+            outlier_mask = np.logical_not(inlier_mask)
+
+            # Predict data of estimated models
+            line_X = np.arange(X.min(), X.max())[:, np.newaxis]
+            line_y_ransac = ransac.predict(add_square_feature(line_X))
+
+            for i in range(len(line_X)-1):
+                cv2.line(img_bev, (int(-line_y_ransac[i]*10)+350, -int(line_X[i]*10)+799), (int(-line_y_ransac[i+1]*10)+350, -int(line_X[i+1]*10)+799), (0,255,0), 3)
+
+        cv2.imwrite(SBEV_PATH+img_name[:-4]+"_bev.png", img_bev)
+
+        
+        end_time = time.time()
+        time_cost.append(end_time - start_time)
+
+    print ("Mean_time_cost:", np.mean(time_cost))
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
